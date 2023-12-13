4a4843453ccd2829ce8448c1567193589894ee88
Vladimir <markoradic15646@gmail.com>
2023-12-13 22:28:14 +0100

    Initial commit

--- web.py
+++ web.py
@@ -22,7 +22,7 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="Add new todo...", placeholder="Add new todo...",
+st.text_input(label="1", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
 
 print("Hello")
